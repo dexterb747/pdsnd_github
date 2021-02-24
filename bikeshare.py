0be16d2e79e353fdc1630c97147c5111b45ffa2b
Dexter Bujan <dexterbujan@hotmail.com>
2021-02-23 21:58:51 -0400

    Updated Global object comments

--- bikeshare.py
+++ bikeshare.py
@@ -2,7 +2,7 @@ import time
 import pandas as pd
 import numpy as np
 
-# define global objects
+# define global objects. These global objects can be used by other local functions.
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
